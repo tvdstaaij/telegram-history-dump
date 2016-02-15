5fe9ba5fb9585e626a46943bb1ba82e4c35285a1
Tim van der Staaij <tvdstaaij@users.noreply.github.com>
2016-02-15 23:07:28 +0100

    Truncate dialog name to 220 bytes (#7)

--- lib/util.rb
+++ lib/util.rb
@@ -2,7 +2,9 @@ require 'pathname'
 require_relative './dump_prepender'
 
 def get_safe_name(name)
-  name.gsub(/([\/\\<>:"|?*]|[^\u0021-\uFFFF])/, $config['character_substitute'])
+  bad_characters = /([\/\\<>:"|?*]|[^\u0021-\uFFFF])/
+  clean_name = name.gsub(bad_characters, $config['character_substitute'])
+  truncate_to_bytesize(clean_name, 220, '_[truncated]')
 end
 
 def get_full_name(user)
@@ -28,4 +30,14 @@ end
 
 def relativize_output_path(path)
   Pathname(path).relative_path_from(Pathname(get_backup_dir)).to_s
+end
+
+def truncate_to_bytesize(str, size, ellipsis = '')
+  str.each_char.each_with_object('') do |char,result|
+    if result.bytesize + char.bytesize > (size - ellipsis.bytesize)
+      break result + ellipsis
+    else
+      result << char
+    end
+  end
 end
