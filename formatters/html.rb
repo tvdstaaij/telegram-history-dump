1bddd4532576bbfc99ca0d6b0ebd4fbfb7537c29
Luc Gommans <git-client@lgms.nl>
2016-02-28 22:48:55 +0100

    HTML link parser improvement: subdomains can start with digits and be digits-only, e.g. http://2.example.com/. Changed regex to look for this.

--- formatters/html.rb
+++ formatters/html.rb
@@ -238,7 +238,7 @@ class HtmlFormatter < FormatterBase
 
     # TODO: IP addresses. IPv6 addresses. I completely forgot about those.
 
-    urls = text.scan(/(^|\s|<)(([a-zA-Z]{1,25}:)?([^@\s]{1,200}@)?(\/\/)?([a-zA-Z][a-zA-Z0-9-]{0,63}\.){0,125}[a-zA-Z][a-zA-Z0-9-]{0,63}\.([a-zA-Z]{2,63}|xn--[a-zA-Z0-9]{1,60})(:[1-9][0-9]{0,4})?(\/[!-~]*)?(\s|>|$|\)|\.))/)
+    urls = text.scan(/(^|\s|<)(([a-zA-Z]{1,25}:)?([^@\s]{1,200}@)?(\/\/)?([0-9a-zA-Z][a-zA-Z0-9-]{0,63}\.){0,125}[a-zA-Z][a-zA-Z0-9-]{0,63}\.([a-zA-Z]{2,63}|xn--[a-zA-Z0-9]{1,60})(:[1-9][0-9]{0,4})?(\/[!-~]*)?(\s|>|$|\)|\.))/)
 
     text = CGI::escapeHTML(text)
 
