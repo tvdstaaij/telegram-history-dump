0b4d0069781506be8b6e951eb61b2ee05844c552
Tim van der Staaij <tvdstaaij@users.noreply.github.com>
2016-11-11 23:21:58 +0100

    Add options for controlling json file naming scheme (#28)

--- dumpers/lib/single_file_line_dumper.rb
+++ dumpers/lib/single_file_line_dumper.rb
@@ -5,25 +5,42 @@ class SingleFileLineDumper < DumperInterface
 
   def start_dialog(dialog, progress)
     @prepender = nil
+    @rename_to = nil
+    @output_dir = File.join(get_backup_dir, get_output_type)
     @state = progress.dumper_state ? progress.dumper_state.clone : {}
-    output_file = @state['outfile']
-    if output_file
-      output_file = File.join(get_backup_dir, output_file)
-      @prepender = DumpPrepender.new(output_file)
+    output_basename = $config['friendly_data_filenames'] == false ?
+      dialog['id'].to_s : get_safe_name(dialog['print_name'])
+    output_filename = output_basename + get_file_extension
+    @current_outfile = @state['outfile']
+    if @current_outfile
+      current_basename = File.basename(@current_outfile, get_file_extension)
+      @rename_to = output_filename if current_basename != output_basename
+      @current_outfile = File.join(get_backup_dir, @current_outfile)
+      @prepender = DumpPrepender.new(@current_outfile)
     else
-      filename = get_safe_name(dialog['print_name']) + get_file_extension
-      output_dir = File.join(get_backup_dir, get_output_type)
-      FileUtils.mkdir_p(output_dir)
-      output_file = File.join(output_dir, filename)
-      @state['outfile'] = relativize_output_path(output_file)
+      FileUtils.mkdir_p(@output_dir)
+      @current_outfile = File.join(@output_dir, output_filename)
+      @state['outfile'] = relativize_output_path(@current_outfile)
     end
-    @stream = File.open(output_file, 'w:UTF-8')
+    @stream = File.open(@current_outfile, 'w:UTF-8')
   end
 
   def end_dialog(dialog)
     @stream.close
     @stream = nil
     @prepender.merge if @prepender
+    if @rename_to && $config['update_data_filenames']
+      new_outfile = File.join(@output_dir, @rename_to)
+      begin
+        FileUtils.mv(@current_outfile, new_outfile)
+        @state['outfile'] = relativize_output_path(new_outfile)
+      rescue SystemCallError => e
+        $log.error('Could not rename %s to %s (error %d), '\
+                   'leaving filename intact' % [
+          @state['outfile'], relativize_output_path(new_outfile), e.errno
+        ])
+      end
+    end
     @state
   end
 
